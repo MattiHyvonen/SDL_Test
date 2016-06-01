b3f697457660a9681ee82e1fd120a9b9c28b6b56
Matti <mattihyvonen@gmail.com>
2016-06-01 21:21:26 +0300

    vektorien ero

--- random.h
+++ random.h
@@ -1,15 +1,17 @@
 #include <stdlib.h>
+#include <vector>
+#include <math.h>
 
-float randf(float min, float max);
-
-void bound(float& value, float min, float max);
-void wrap(float& value, float min, float max);
-
+float fabs(float f) {
+	if (f > 0) return f;
+	else return (f*(-1));
+}
 
 
 float randf(float min, float max) {
 	return ((float)rand() / RAND_MAX) * (max - min) + min;
 }
+
 
 void bound(float& value, float min, float max) {
 	if (value < min) value = min;
@@ -21,4 +23,20 @@ void wrap(float& value, float min, float max) {
 	float w = max - min;
 	while (value < min) value += w;
 	while (value > max) value -= w;
+}
+
+
+float vektorienEro(std::vector<float>& A, std::vector<float>& B) {	
+
+	if (A.size() == B.size() || A.empty() || B.empty() )
+		return -1;
+
+	float summa = 0;
+
+	for (int i = 0; i < A.size(); i++) {
+		summa += fabs(A[i] - B[i]);	
+	}
+
+	summa /= A.size();
+	return summa;
 }
